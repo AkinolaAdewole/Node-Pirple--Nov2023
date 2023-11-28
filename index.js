44de6277ce6d6ca9f2f10f5fa57949367be2e7c5
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-28 23:11:33 +0100

    Pirple Node

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const http = require ('http');
 const server = http.createServer((req,res)=>{
     res.end('Hello World\n')
+});
+
+
+const port = 3200
+server.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
 })

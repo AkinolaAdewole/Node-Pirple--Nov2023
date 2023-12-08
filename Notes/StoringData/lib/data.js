34de7ae1d45e45b90550cf4816f019a46f30d258
Akinola Adewole <adewoleakinola24@gmail.com>
2023-12-08 22:22:48 +0100

    Backend (Node)

--- Notes/StoringData/lib/data.js
+++ Notes/StoringData/lib/data.js
@@ -14,4 +14,11 @@ const lib = {}
 lib.baseDir = path.join(__dirname,'/../.data/');
 
 // Write data to a file
-lib.create = (dir,file, data, callback)={}
+lib.create = (dir,file, data, callback)=>{
+    // Open the file for writing
+    fs.open(lib.baseDir+dir+'/'+file+'.json','wx', (err, fileDescriptor)=>{
+        if(!err && fileDescriptor){
+            //convert data to string
+        }
+    })
+}

2751254c951c48ba75a1d9d5728bea49cb1c8684
Akinola Adewole <adewoleakinola24@gmail.com>
2025-03-03 23:04:30 +0100

    Node.js playground

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,47 +1,81 @@
-const {res}=require('express')
-const express = require('express')
-const app =express()
-const {req}=require('http')
-const bodyParser=require('body-parser')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const todoModel = require('../model/todoModel');
 
+let data = [{ item: 'draw' }, { item: 'code' }];
 
-const data=[{item:"draw"}, {item:"code"}]
-// app.use(bodyParser.urlencoded({ extended: false })) 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const method = req.method;
+    
+    // GET: Fetch all todos
+    if (req.url === '/todo' && method === 'GET') {
+        todoModel.find({}, (err, todos) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                return res.end(JSON.stringify({ message: 'Error fetching todos' }));
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(todos));
+        });
+    }
 
-const todoModel=require('../model/todoModel')
+    // POST: Add a new todo
+    else if (req.url === '/todo' && method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
 
+        req.on('end', () => {
+            const { item } = JSON.parse(body);
+            if (!item) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                return res.end(JSON.stringify({ message: 'Item is required' }));
+            }
 
+            todoModel.create({ item }, (err, newTodo) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    return res.end(JSON.stringify({ message: 'Error saving todo' }));
+                }
+                data.push({ item });
 
-  const addTodo=()=>{
-    const{item}=request.body
-      todoModel.create({item}, (err,res)=>{
-        
-        if(err){
-            console.log(err);
-            console.log("There us error");
-        }else{
-            console.log('saved')
-        }
-    })
-    data.push({item}) 
-  }
-  
+                res.writeHead(201, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: 'Todo added', todo: newTodo }));
+            });
+        });
+    }
 
+    // DELETE: Remove a todo by ID
+    else if (parsedUrl.pathname.startsWith('/todo/') && method === 'DELETE') {
+        const id = parsedUrl.pathname.split('/')[2]; // Extract ID from URL
 
-const getTodo=(request, res)=>{
-  todoModel.find((err,res)=>{
-      res.send(json.stringify(res))
-  })}
+        todoModel.findByIdAndDelete(id, (err, deletedTodo) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                return res.end(JSON.stringify({ message: 'Error deleting todo' }));
+            }
+            if (!deletedTodo) {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                return res.end(JSON.stringify({ message: 'Todo not found' }));
+            }
 
-    app.get('/todo',(req,res)=>{
-        res.render("pages/todo", {todos:data}) 
-       })
-      
-       
-      
-      app.delete('/todo',(req,res)=>{
-          
-      })
-module.exports={getTodo, addTodo}
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Todo deleted', todo: deletedTodo }));
+        });
+    }
 
+    // Default 404
+    else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Route not found' }));
+    }
+});
+
+// Start server
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
